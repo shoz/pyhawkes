5b84cf5e0e2146e99975902947873523600ab88d
Shoji Ihara <shoji.ihara@gmail.com>
2015-02-23 23:31:50 +0900

    added info

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 setup(
+    name='pyhawkes',
+    version='0.0.1',
+    author='Shoji Ihara',
+    author_email='shoji.ihara@gmail.com'
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension("hawkes_process", ["pyhawkes/hawkes_process.pyx"])],
 )
